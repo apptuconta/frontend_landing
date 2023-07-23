de1e8fb0b38b2a05304a2540eb6401dbcb0b2918
Schaffer <Gustavo.Schaffer@comafi.com.ar>
2023-07-23 02:38:45 -0300

    desarrollo pantalla soy contador en construccion

---
+++ src/app/pages/soy-contador/registrar-contador/registrar-contador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-contador',
+  templateUrl: './registrar-contador.component.html',
+  styleUrls: ['./registrar-contador.component.css']
+})
+export class RegistrarContadorComponent {
+
+}
