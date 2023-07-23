0df6b007714e1976fa14f2df30eb38c2d4583a35
Schaffer <Gustavo.Schaffer@comafi.com.ar>
2023-07-23 02:16:40 -0300

    desarrollo pantalla busqueda de contador waiting

---
+++ src/app/pages/necesito-contador/buscar-contador/buscar-contador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-contador',
+  templateUrl: './buscar-contador.component.html',
+  styleUrls: ['./buscar-contador.component.css']
+})
+export class BuscarContadorComponent {
+
+}
